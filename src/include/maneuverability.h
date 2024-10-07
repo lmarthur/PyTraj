0053e44ccc6c01f7e26aa9c52e6f996046bbb3f5
Lucas Arthur <lmarthur@dhcp-10-29-232-62.dyn.MIT.EDU>
2024-10-07 06:49:57 -0700

    perfect_maneuv and tests

--- src/include/maneuverability.h
+++ src/include/maneuverability.h
@@ -37,4 +37,41 @@ state instant_maneuv(state *true_state, cart_vector *a_command){
     return updated_state;
 }
 
+state perfect_maneuv(state *true_state, state *estimated_state, state *desired_state){
+    /*
+    Simulates perfect maneuverability by shifting the true state by the difference between the estimated and desired states
+
+    INPUTS:
+    ----------
+        true_state: state *
+            pointer to the true state of the vehicle
+        estimated_state: state *
+            pointer to the estimated state of the vehicle
+        desired_state: state *
+            pointer to the desired state of the vehicle
+
+    OUTPUTS:
+    ----------
+        state: updated_state
+            true state of the vehicle after the maneuver
+    */
+
+    // Initialize the new state
+    state updated_state = *true_state;
+
+    // Calculate the difference between the desired and estimated states
+    updated_state.x = true_state->x + (desired_state->x - estimated_state->x);
+    updated_state.y = true_state->y + (desired_state->y - estimated_state->y);
+    updated_state.z = true_state->z + (desired_state->z - estimated_state->z);
+    updated_state.vx = true_state->vx + (desired_state->vx - estimated_state->vx);
+    updated_state.vy = true_state->vy + (desired_state->vy - estimated_state->vy);
+    updated_state.vz = true_state->vz + (desired_state->vz - estimated_state->vz);
+    updated_state.theta_lat = true_state->theta_lat + (desired_state->theta_lat - estimated_state->theta_lat);
+    updated_state.theta_long = true_state->theta_long + (desired_state->theta_long - estimated_state->theta_long);
+    updated_state.ax_total = true_state->ax_total + (desired_state->ax_total - estimated_state->ax_total);
+    updated_state.ay_total = true_state->ay_total + (desired_state->ay_total - estimated_state->ay_total);
+    updated_state.az_total = true_state->az_total + (desired_state->az_total - estimated_state->az_total);
+
+    return updated_state;
+}
 #endif
--- test/maneuverability_test.h
+++ test/maneuverability_test.h
@@ -46,3 +46,64 @@ TEST(maneuverability, instant_maneuv){
     REQUIRE_EQ(true_state.az_total, true_state.az_grav + true_state.az_drag + true_state.az_lift + true_state.az_thrust);
 
 }
+
+TEST(maneuverability, perfect_maneuv){
+    // initialize the true state
+    state true_state;
+    true_state.x = 6371e3 + 10;
+    true_state.y = 0;
+    true_state.z = 0;
+    true_state.theta_lat = 0;
+    true_state.vx = -1;
+    true_state.vy = 1;
+    true_state.vz = 0;
+    true_state.ax_grav = 0;
+    true_state.ay_grav = 0;
+    true_state.az_grav = 0;
+    true_state.ax_drag = 0;
+    true_state.ay_drag = 0;
+    true_state.az_drag = 0;
+    true_state.ax_lift = 0;
+    true_state.ay_lift = 0;
+    true_state.az_lift = 0;
+    true_state.ax_thrust = 0;
+    true_state.ay_thrust = 0;
+    true_state.az_thrust = 0;
+
+    state estimated_state = true_state;
+    state desired_state = true_state;
+
+    // For desired = estimated, the true state should not change
+    state new_true_state = perfect_maneuv(&true_state, &estimated_state, &desired_state);
+    REQUIRE_EQ(new_true_state.x, true_state.x);
+    REQUIRE_EQ(new_true_state.y, true_state.y);
+    REQUIRE_EQ(new_true_state.z, true_state.z);
+    REQUIRE_EQ(new_true_state.vx, true_state.vx);
+    REQUIRE_EQ(new_true_state.vy, true_state.vy);
+    REQUIRE_EQ(new_true_state.vz, true_state.vz);
+    REQUIRE_EQ(new_true_state.ax_grav, true_state.ax_grav);
+    REQUIRE_EQ(new_true_state.ay_grav, true_state.ay_grav);
+    REQUIRE_EQ(new_true_state.az_grav, true_state.az_grav);
+    REQUIRE_EQ(new_true_state.ax_drag, true_state.ax_drag);
+    REQUIRE_EQ(new_true_state.ay_drag, true_state.ay_drag);
+    REQUIRE_EQ(new_true_state.az_drag, true_state.az_drag);
+    REQUIRE_EQ(new_true_state.ax_lift, true_state.ax_lift);
+    REQUIRE_EQ(new_true_state.ay_lift, true_state.ay_lift);
+    REQUIRE_EQ(new_true_state.az_lift, true_state.az_lift);
+    REQUIRE_EQ(new_true_state.ax_thrust, true_state.ax_thrust);
+    REQUIRE_EQ(new_true_state.ay_thrust, true_state.ay_thrust);
+    REQUIRE_EQ(new_true_state.az_thrust, true_state.az_thrust);
+
+    // For desired.x = estimated.x + 1, the true state should change by 1
+    desired_state.x = estimated_state.x + 1;
+    new_true_state = perfect_maneuv(&true_state, &estimated_state, &desired_state);
+    REQUIRE_EQ(new_true_state.x, true_state.x + 1);
+    REQUIRE_EQ(new_true_state.y, true_state.y);
+    REQUIRE_EQ(new_true_state.z, true_state.z);
+
+    // For desired.y = estimated.y - 1, the true state should change by 1
+    desired_state.y = estimated_state.y - 1;
+    new_true_state = perfect_maneuv(&true_state, &estimated_state, &desired_state);
+    REQUIRE_EQ(new_true_state.y, true_state.y - 1);
+
+}
